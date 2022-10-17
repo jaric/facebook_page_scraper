243cfa2418ff2ee65cadd7975974ca16575d8d7e
Jaric Kuo <jaric.kuo@mile.cloud>
2022-10-17 16:25:58 +0800

    make firefox faster

--- facebook_page_scraper/driver_initialization.py
+++ facebook_page_scraper/driver_initialization.py
@@ -68,6 +68,43 @@ class Initializer:
 
             return webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=self.set_properties(browser_option))
         elif browser_name.lower() == "firefox":
+
+            profile = webdriver.FirefoxProfile()
+            profile.set_preference("network.http.pipelining", True)
+            profile.set_preference("network.http.proxy.pipelining", True)
+            profile.set_preference("network.http.pipelining.maxrequests", 8)
+            profile.set_preference("content.notify.interval", 500000)
+            profile.set_preference("content.notify.ontimer", True)
+            profile.set_preference("content.switch.threshold", 250000)
+            profile.set_preference("browser.cache.memory.capacity", 65536)  # Increase the cache capacity.
+            profile.set_preference("browser.startup.homepage", "about:blank")
+            profile.set_preference("reader.parse-on-load.enabled", False)  # Disable reader, we won't need that.
+            profile.set_preference("browser.pocket.enabled", False)  # Duck pocket too!
+            profile.set_preference("loop.enabled", False)
+            profile.set_preference("browser.chrome.toolbar_style", 1)  # Text on Toolbar instead of icons
+            profile.set_preference("browser.display.show_image_placeholders",
+                                   False)  # Don't show thumbnails on not loaded images.
+            profile.set_preference("browser.display.use_document_colors", False)  # Don't show document colors.
+            profile.set_preference("browser.display.use_document_fonts", 0)  # Don't load document fonts.
+            profile.set_preference("browser.display.use_system_colors", True)  # Use system colors.
+            profile.set_preference("browser.formfill.enable", False)  # Autofill on forms disabled.
+            profile.set_preference("browser.helperApps.deleteTempFileOnExit", True)  # Delete temprorary files.
+            profile.set_preference("browser.shell.checkDefaultBrowser", False)
+            profile.set_preference("browser.startup.homepage", "about:blank")
+            profile.set_preference("browser.startup.page", 0)  # blank
+            profile.set_preference("browser.tabs.forceHide", True)  # Disable tabs, We won't need that.
+            profile.set_preference("browser.urlbar.autoFill", False)  # Disable autofill on URL bar.
+            profile.set_preference("browser.urlbar.autocomplete.enabled", False)  # Disable autocomplete on URL bar.
+            profile.set_preference("browser.urlbar.showPopup", False)  # Disable list of URLs when typing on URL bar.
+            profile.set_preference("browser.urlbar.showSearch", False)  # Disable search bar.
+            profile.set_preference("extensions.checkCompatibility", False)  # Addon update disabled
+            profile.set_preference("extensions.checkUpdateSecurity", False)
+            profile.set_preference("extensions.update.autoUpdateEnabled", False)
+            profile.set_preference("extensions.update.enabled", False)
+            profile.set_preference("general.startup.browser", False)
+            profile.set_preference("plugin.default_plugin_disabled", False)
+            profile.set_preference("permissions.default.image", 2)  # Image load disabled again
+
             browser_option = FirefoxOptions()
             if self.proxy is not None:
                 options = {
@@ -77,7 +114,7 @@ class Initializer:
                 }
                 logger.info("Using: {}".format(self.proxy))
                 return webdriver.Firefox(executable_path=GeckoDriverManager().install(),
-                                         options=self.set_properties(browser_option), seleniumwire_options=options)
+                                         options=self.set_properties(browser_option), seleniumwire_options=options,  firefox_profile=profile)
 
             # automatically installs geckodriver and initialize it and returns the instance
             return webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=self.set_properties(browser_option))
